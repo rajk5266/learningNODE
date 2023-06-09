e63767aa1744751a885fddadf6e6f043c4e9f37d
rajk5266 <kushwaharaj903@gmail.com>
2023-06-09 08:25:48 +0530

    showing stored data on screen

--- app.js
+++ app.js
@@ -1,57 +1,86 @@
-// const http = require('http');
-
-// const server = http.createServer((req, res) =>{
-//     // console.log(req)
-//     console.log('RajKushwaha')
-// });
-
-// // server.listen(4000)
-
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  // console.log('Raj Kushwaha')
-
   const url = req.url;
-  if(url == '/home'){
-    res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title> hello from node.js </title></head>');
-  res.write('<body><h1> WELCOME HOME</h1></body>');
-  res.write('</html>')
-  return res.end()
-  }
-  else if(url == '/Aboutus'){
-    res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title> hello from node.js </title></head>');
-  res.write('<body><h1> Welcome to About Us page </h1></body>');
-  res.write('</html>')
-  return res.end()
-  }
-  else if(url == '/node'){
-    res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title> hello from node.js </title></head>');
-  res.write('<body><h1> Welcome to my Node.js project </h1></body>');
-  res.write('</html>')
-  return res.end()
-  }
+  const method = req.method;
 
-  console.log(req.url, req.method, req.headers)
-  
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title> hello from node.js </title></head>');
-  res.write('<body><h1> hello </h1></body>');
-  res.write('</html>')
-  res.end()
+  if (url === '/') {
+    fs.readFile('message.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err)
+        res.statusCode = 500
+        res.end('error while loading')
+      } 
+      else {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Display Message</title></head>')
+        res.write('<body>')
+        res.write('<h1>' + data + '</h1>')
+        res.write('<hr>')
+        res.write('<form action="/message" method="POST">')
+        res.write('<input type="text" name="message">')
+        res.write('<button type="submit">Send</button>')
+        res.write('</form>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end()
+    }
+    });
+  }
+   else if (url === '/message' && method === 'POST') {
+    const body = []
+    req.on('data', (chunk) =>{
+      console.log(chunk)
+      body.push(chunk)
+    })
+   return req.on('end', () =>{
+      const parsedbody = Buffer.concat(body).toString();
+      const message = parsedbody.split('=')[1];
+      console.log(message)
+      fs.writeFile('message.txt', message, (error) =>{
 
-  // res.end('RAJ KUSHWAHA')
-  // process.exit()
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end()
+      });
+    })
+  } 
+  else if (url === '/home') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> hello from node.js </title></head>');
+    res.write('<body><h1> WELCOME HOME</h1></body>');
+    res.write('</html>')
+    return res.end()
+  } 
+  else if (url === '/Aboutus') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> hello from node.js </title></head>');
+    res.write('<body><h1> welcoe to about us page</h1></body>');
+    res.write('</html>')
+    return res.end()
+  }
+   else if (url === '/node') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> hello from node.js </title></head>');
+    res.write('<body><h1> welcome to my node.js projecr</h1></body>');
+    res.write('</html>')
+    return res.end()
+  }
+   else {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>hello from node.js</title></head>');
+    res.write('<body><h1>hello</h1></body>');
+    res.write('</html>');
+    res.end();
+  }
 });
 
 server.listen(4000, () => {
-  console.log('Server is running on port 4000')
+  console.log('Server is running on port 4000');
 });
-
