2027dc5a8f8bb89d3b80ab7dac19468b6e44e981
rajk5266 <kushwaharaj903@gmail.com>
2023-06-11 02:12:59 +0530

    used express, with 2 middlewares and app.listen()

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const http = require('http');
-const routes = require('./routes.js');
+// const http = require('http');
+// const routes = require('./routes.js');
+const express = require('express');
 
-// const server = http.createServer(routes)
-const server = http.createServer(routes.handler)
-console.log(routes.sometext)
-server.listen(4000, () => {
-  console.log('Server is running on port 4000');
-});
+const app = express()
 
+app.use((req, res, next) =>{
+  console.log('in the middleware')
+  next()
+  // next() allows the request to  continue to the next middleware in line
+})
+app.use((req, res, next) =>{
+  console.log('in the another middleware')
+  // responses we can send
+  res.send({key: "value"})
+})
+
+
+// const server = http.createServer(app)
+// server.listen(4000, () => {
+//   console.log('Server is running on port 4000');
+// });
+
+app.listen(4000)
