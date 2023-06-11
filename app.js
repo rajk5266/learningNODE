042ddaaf848cd61ea66b9f36832930f603113cc8
rajk5266 <kushwaharaj903@gmail.com>
2023-06-11 13:19:54 +0530

    parsed input

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-// const http = require('http');
-// const routes = require('./routes.js');
-const express = require('express');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 
-app.use((req, res, next) =>{
-  console.log('in the middleware')
-  next()
-  // next() allows the request to  continue to the next middleware in line
-})
-app.use((req, res, next) =>{
-  console.log('in the another middleware')
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use( '/add-product', (req, res, next) =>{
+  // console.log('in the another middleware')
   // responses we can send
-  res.send({key: "value"})
+  res.send('<form action="/product" method="POST"><input type="text" name="product"> <input type="text" name="size" placeholder="Enter Size"> <button type="submit">Add Product</button></form>')
 })
 
+app.post('/product', (req,res,next) =>{
+  console.log(req.body);
+  res.redirect('/')
+})
+app.use( '/', (req, res, next) =>{
+  res.send('<h1>hello from Express<h1/>')
+})
 
-// const server = http.createServer(app)
-// server.listen(4000, () => {
-//   console.log('Server is running on port 4000');
-// });
 
 app.listen(4000)
