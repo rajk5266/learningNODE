d9156ec1ca3eca8ff0f1a96412f261cf51c9faa9
rajk5266 <kushwaharaj903@gmail.com>
2023-06-28 15:06:40 +0530

    one-to-many|| many-to-many

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error.js')
-
 const sequelize = require('./util/database.js')
+const Product = require('./models/product.js')
+const User = require('./models/user.js')
 
 const app = express()
 
@@ -19,17 +20,37 @@ const SUCCESSroutes = require('./routes/successroutes.js')
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) =>{
+    User.findByPk(1)
+    .then(user =>{
+        req.user = user;
+        next()
+    })
+    .catch(err => console.log(err))
+})
+
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use('/admin' , contactRoutes)
 app.use('/admin',SUCCESSroutes)
 app.use(errorController.get404)
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
 sequelize
+//  .sync( {force: true})
  .sync( )
  .then(result => {
-    // console.log(result)
-    app.listen(4000) 
+    return User.findByPk(1)
+     })
+     .then(user =>{
+        if(!user){
+            return User.create({name: 'raj', email: "dummyemail2h.com"})
+        }
+        return user;
+     })
+     .then(user =>{
+        app.listen(4000)
      })
  .catch(err => console.log(err))
 
